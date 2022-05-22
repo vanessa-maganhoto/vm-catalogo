94b589d976bc4416d8c74e70822a7ca5ad0b7928
vanessa-maganhoto <vanessammaganhoto@gmail.com>
2022-05-22 20:29:56 -0300

    Criação do projeto: backend

---
+++ backend/src/main/java/com/vanessamatos/vmcatalogo/VmcatalogoApplication.java
@@ -0,0 +1,13 @@
+package com.vanessamatos.vmcatalogo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VmcatalogoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VmcatalogoApplication.class, args);
+	}
+
+}
