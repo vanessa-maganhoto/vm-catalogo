35c0571443e6cd4af6b22dd7b4921e273e76d781
vanessa-maganhoto <vanessammaganhoto@gmail.com>
2022-05-23 14:03:52 -0300

    CategoryResource

---
+++ backend/src/main/java/com/vanessamatos/vmcatalogo/resources/CategoryResource.java
@@ -0,0 +1,23 @@
+package com.vanessamatos.vmcatalogo.resources;
+
+import com.vanessamatos.vmcatalogo.entities.Category;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/categories")
+public class CategoryResource {
+
+    @GetMapping
+    public ResponseEntity<List<Category>> findAll(){
+        List<Category> list = new ArrayList<>();
+        list.add(new Category(1L, "Books"));
+        list.add(new Category(2L, "electronics"));
+        return ResponseEntity.ok().body(list);
+    }
+}
