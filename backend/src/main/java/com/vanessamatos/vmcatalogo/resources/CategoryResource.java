74dd76b52b3205bb6d1b1a841f111af6d4073f08
vanessa-maganhoto <vanessammaganhoto@gmail.com>
2022-05-24 16:50:46 -0300

    Product

--- backend/src/main/java/com/vanessamatos/vmcatalogo/resources/CategoryResource.java
+++ backend/src/main/java/com/vanessamatos/vmcatalogo/resources/CategoryResource.java
@@ -4,6 +4,9 @@ import com.vanessamatos.vmcatalogo.dto.CategoryDTO;
 import com.vanessamatos.vmcatalogo.entities.Category;
 import com.vanessamatos.vmcatalogo.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -19,8 +22,16 @@ public class CategoryResource {
     @Autowired
     private CategoryService categoryService;
     @GetMapping
-    public ResponseEntity<List<CategoryDTO>> findAll(){
-        List<CategoryDTO> list = categoryService.findAll();
+    public ResponseEntity<Page<CategoryDTO>> findAll(
+            @RequestParam(value = "page", defaultValue = "0") Integer page,
+            @RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
+            @RequestParam(value = "direction", defaultValue = "ASC") String direction,
+            @RequestParam(value = "orderBy", defaultValue = "name") String orderBy
+
+    ){
+        PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+
+        Page<CategoryDTO> list = categoryService.findAllPaged(pageRequest);
         return ResponseEntity.ok().body(list);
     }
 
