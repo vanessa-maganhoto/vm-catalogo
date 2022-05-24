8e402f63e5aa45849cdb1e7a1406220e1565c9f5
vanessa-maganhoto <vanessammaganhoto@gmail.com>
2022-05-24 15:13:45 -0300

    Dados de auditoria

--- backend/src/main/java/com/vanessamatos/vmcatalogo/entities/Category.java
+++ backend/src/main/java/com/vanessamatos/vmcatalogo/entities/Category.java
@@ -2,6 +2,7 @@ package com.vanessamatos.vmcatalogo.entities;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.time.Instant;
 import java.util.Objects;
 @Entity
 @Table(name = "tb_category")
@@ -12,6 +13,11 @@ public class Category implements Serializable {
     private Long id;
     private String name;
 
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant createdAt;
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant updatedAt;
     public Category(){}
 
     public Category(Long id, String name) {
@@ -33,6 +39,24 @@ public class Category implements Serializable {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public Instant getUpdatedAt() {
+        return updatedAt;
+    }
+
+    @PrePersist
+    public void prePersist(){
+         createdAt = Instant.now();
+    }
+
+    @PreUpdate
+    public void preUpdate(){
+        updatedAt = Instant.now();
     }
 
     @Override
