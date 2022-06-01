e89069777770906e3597bc6c36093e41a06db654
vanessa-maganhoto <vanessammaganhoto@gmail.com>
2022-06-01 14:11:05 -0300

    Seed BD

--- backend/src/main/java/com/vanessamatos/vmcatalogo/entities/User.java
+++ backend/src/main/java/com/vanessamatos/vmcatalogo/entities/User.java
@@ -1,19 +1,28 @@
 package com.vanessamatos.vmcatalogo.entities;
 
+import javax.persistence.*;
 import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
-
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
     private static final long serialVersionUID= 1L;
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String firstName;
     private String lastName;
     private String email;
     private String password;
 
+    @ManyToMany
+    @JoinTable(
+            name = "tb_user_role",
+            joinColumns = @JoinColumn(name = "user_id"), // O @JoinColumn de joinColumns será referenciando a entidade PRODUTO
+            inverseJoinColumns = @JoinColumn(name = "role_id") // O inverseJoinClumns "pega" o nome que está na coleção
+    )
     private Set<Role> roles = new HashSet<>();
 
     public User(){}
--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -1,3 +1,14 @@
+INSERT INTO tb_user (first_name, last_name, email, password) VALUES ('Samuel', 'Barros', 'samuelbarros@gmail.com', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG');
+INSERT INTO tb_user (first_name, last_name, email, password) VALUES ('Ana', 'Maria', 'anamaria@gmail.com', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG');
+
+INSERT INTO tb_role (authority) VALUES ('ROLE_OPERATOR');
+INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
+
+INSERT INTO tb_user_role (user_id, role_id) VALUES (1, 1);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 1);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
+
+
 INSERT INTO tb_category (name, created_At, deleted) VALUES ('Livros', NOW(), false);
 INSERT INTO tb_category (name, created_At, deleted) VALUES ('Eletrônicos', NOW(), false);
 INSERT INTO tb_category (name, created_At, deleted) VALUES ('Computadores', NOW(), false);
