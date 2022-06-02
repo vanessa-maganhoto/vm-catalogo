e9edd5c19bb5dabd82369e73c6fecd31b367652d
vanessa-maganhoto <vanessammaganhoto@gmail.com>
2022-06-02 15:02:12 -0300

    Validação customizada com Bean Validation

--- backend/src/main/java/com/vanessamatos/vmcatalogo/dto/UserInsertDTO.java
+++ backend/src/main/java/com/vanessamatos/vmcatalogo/dto/UserInsertDTO.java
@@ -1,5 +1,8 @@
 package com.vanessamatos.vmcatalogo.dto;
 
+import com.vanessamatos.vmcatalogo.services.validation.UserInsertValid;
+
+@UserInsertValid
 public class UserInsertDTO extends UserDTO{
     private static final long serialVersionUID = 1L;
 
