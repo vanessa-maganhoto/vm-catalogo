02b2641530f4476ec2cbe7b47ae3c92a9e1cb486
vanessa-maganhoto <vanessammaganhoto@gmail.com>
2022-06-03 14:55:56 -0300

    Implementação do OAuth2 autorization server

--- backend/src/main/java/com/vanessamatos/vmcatalogo/services/UserService.java
+++ backend/src/main/java/com/vanessamatos/vmcatalogo/services/UserService.java
@@ -7,7 +7,6 @@ import com.vanessamatos.vmcatalogo.entities.User;
 import com.vanessamatos.vmcatalogo.repositories.CategoryRepository;
 import com.vanessamatos.vmcatalogo.repositories.RoleRepository;
 import com.vanessamatos.vmcatalogo.repositories.UserRepository;
-import com.vanessamatos.vmcatalogo.repositories.UserRepository;
 import com.vanessamatos.vmcatalogo.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
@@ -15,16 +14,22 @@ import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.persistence.EntityNotFoundException;
 import java.util.Optional;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 
+    private static Logger logger = LoggerFactory.getLogger(UserService.class);
     @Autowired
     private UserRepository userRepository;
 
@@ -94,5 +99,17 @@ public class UserService {
             Role role = roleRepository.getById(roleDTO.getId());
             newUser.getRoles().add(role);
         }
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        User user = userRepository.findByEmail(username);
+        if (user == null) {
+            logger.error("User not found: " + username);
+            throw new UsernameNotFoundException("Email not found");
+        }
+        logger.info("User found: " + username);
+        return user;
     }
 }
