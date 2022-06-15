6786696fe172633222d8c570b7253b7373a75cf1
vanessa-maganhoto <vanessammaganhoto@gmail.com>
2022-06-15 15:13:59 -0300

    Refatoração das classes Product e Category alterando de Pagerequest para Pageable

--- backend/src/main/java/com/vanessamatos/vmcatalogo/resources/CategoryResource.java
+++ backend/src/main/java/com/vanessamatos/vmcatalogo/resources/CategoryResource.java
@@ -6,6 +6,7 @@ import com.vanessamatos.vmcatalogo.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -22,16 +23,9 @@ public class CategoryResource {
     @Autowired
     private CategoryService categoryService;
     @GetMapping
-    public ResponseEntity<Page<CategoryDTO>> findAll(
-            @RequestParam(value = "page", defaultValue = "0") Integer page,
-            @RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
-            @RequestParam(value = "direction", defaultValue = "ASC") String direction,
-            @RequestParam(value = "orderBy", defaultValue = "name") String orderBy
+    public ResponseEntity<Page<CategoryDTO>> findAll(Pageable pageable){
 
-    ){
-        PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
-
-        Page<CategoryDTO> list = categoryService.findAllPaged(pageRequest);
+        Page<CategoryDTO> list = categoryService.findAllPaged(pageable);
         return ResponseEntity.ok().body(list);
     }
 
--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -1,5 +1,6 @@
 INSERT INTO tb_user (first_name, last_name, email, password) VALUES ('Samuel', 'Barros', 'samuelbarros@gmail.com', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG');
 INSERT INTO tb_user (first_name, last_name, email, password) VALUES ('Ana', 'Maria', 'anamaria@gmail.com', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG');
+INSERT INTO tb_user (first_name, last_name, email, password) VALUES ('Alex', 'Mario', 'alex@gmail.com', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG');
 
 INSERT INTO tb_role (authority) VALUES ('ROLE_OPERATOR');
 INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
@@ -7,6 +8,7 @@ INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
 INSERT INTO tb_user_role (user_id, role_id) VALUES (1, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 
 
 INSERT INTO tb_category (name, created_At, deleted) VALUES ('Livros', NOW(), false);
