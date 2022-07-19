fb99ad33490a27df408bec70c9ec44addfd642da
vanessa-maganhoto <vanessammaganhoto@gmail.com>
2022-07-18 21:38:38 -0300

    Estilização da navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,13 @@
-import './assets/styles/custom.scss'
+import './assets/styles/custom.scss';
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <h1>Vanessa</h1>
-  );
+    <>
+      <Navbar />
+    </>
+  )
 }
 
 export default App;
