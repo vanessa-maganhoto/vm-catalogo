014a4f4d25a5653a69fca6f0cddaf84c62136e71
vanessa-maganhoto <vanessammaganhoto@gmail.com>
2022-07-21 14:58:09 -0300

    FormatPrice

--- frontend/src/components/ProductPrice/index.tsx
+++ frontend/src/components/ProductPrice/index.tsx
@@ -1,3 +1,4 @@
+import { formatPrice } from 'util/formatters';
 import './styles.css';
 
 type Props = {
@@ -10,7 +11,7 @@ const ProductPrice = ( { price } : Props) => {
     return (
         <div className="product-price-container">
             <span>R$</span>
-            <h3>{price}</h3>
+            <h3>{formatPrice(price)}</h3>
         </div>
     );
 }
