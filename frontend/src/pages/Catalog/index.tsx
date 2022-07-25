d71bdf1b125a48c8de7f3475258bc4d67f198a49
vanessa-maganhoto <vanessammaganhoto@gmail.com>
2022-07-25 13:20:51 -0300

    Título da página de catálogo

--- frontend/src/pages/Catalog/index.tsx
+++ frontend/src/pages/Catalog/index.tsx
@@ -1,45 +1,38 @@
 import ProductCard from 'components/ProductCard';
+import { Link } from 'react-router-dom';
 import { Product } from 'types/product';
 
+import './styles.css';
+
 const Catalog = () => {
-  const product : Product = {
+  const product: Product = {
     id: 1,
-    name: "The Lord of the Rings",
-    description: "The Lord of the Rings",
+    name: 'The Lord of the Rings',
+    description: 'The Lord of the Rings',
     price: 90.5,
-    imgUrl: "https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/1-big.jpg",
-    date: "2020-07-13T20:50:07.123450Z",
+    imgUrl:
+      'https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/1-big.jpg',
+    date: '2020-07-13T20:50:07.123450Z',
     categories: [
-        {
-            id: 2,
-            name: "Eletrônicos"
-        }
-    ]
-}
-  
-  return (  
-    <div className="container my-4">
+      {
+        id: 2,
+        name: 'Eletrônicos',
+      },
+    ],
+  };
+
+  return (
+    <div className="container my-4 catalog-container">
+      <div className="row catalog-title-container ">
+        <h1> Catálogo de produtos</h1>
+      </div>
+
       <div className="row">
         <div className="col-sm-6 col-lg-4 col-xl-3">
-          <ProductCard product={product}/>
+          <Link to="/products/1">
+            <ProductCard product={product} />
+          </Link>
         </div>
-
-        <div className="col-sm-6 col-lg-4 col-xl-3">
-          <ProductCard product={product}/>
-        </div>
-
-        <div className="col-sm-6 col-lg-4 col-xl-3">
-          <ProductCard product={product}/>
-        </div>
-
-        <div className="col-sm-6 col-lg-4 col-xl-3">
-          <ProductCard product={product}/>
-        </div>
-
-        <div className="col-sm-6 col-lg-4 col-xl-3">
-          <ProductCard product={product}/>
-        </div>
-        
       </div>
     </div>
   );
--- frontend/src/pages/ProductDetails/index.tsx
+++ frontend/src/pages/ProductDetails/index.tsx
@@ -1,32 +1,42 @@
-import { ReactComponent as ArrowIcon } from 'assets/images/arrow.svg'
+import { ReactComponent as ArrowIcon } from 'assets/images/arrow.svg';
 import ProductPrice from 'components/ProductPrice';
+import { Link } from 'react-router-dom';
 
-import './styles.css'
+import './styles.css';
 
 const ProductDetails = () => {
   return (
     <div className="product-details-container">
       <div className="base-card product-details-card">
-        <div className="goback-container">
-        <ArrowIcon />
-        <h2>VOLTAR</h2>
-        </div>
+        <Link to="/products">
+          <div className="goback-container">
+            <ArrowIcon />
+            <h2>VOLTAR</h2>
+          </div>
+        </Link>
         <div className="row">
           <div className="col-xl-6">
             <div className="img-container">
-                <img src="https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/1-big.jpg" alt="Nome do produto"/>
+              <img
+                src="https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/1-big.jpg"
+                alt="Nome do produto"
+              />
             </div>
 
             <div className="name-price-container">
-                <h1>Nome do produto</h1>
-                <ProductPrice price ={950.60}/>
+              <h1>Nome do produto</h1>
+              <ProductPrice price={950.6} />
             </div>
           </div>
 
           <div className="col-xl-6">
             <div className="description-container">
-                <h2>Descrição do produto</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio, atque dolorem. Ullam magni rem quis at optio minima cum, nulla vero aliquid doloribus! Rem et iure ex non voluptatem officia?</p>
+              <h2>Descrição do produto</h2>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio,
+                atque dolorem. Ullam magni rem quis at optio minima cum, nulla
+                vero aliquid doloribus! Rem et iure ex non voluptatem officia?
+              </p>
             </div>
           </div>
         </div>
