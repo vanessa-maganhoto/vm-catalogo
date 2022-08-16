afc65b76b00c343b90f8b4cb64e0c916f37c0f3b
vanessa-maganhoto <vanessammaganhoto@gmail.com>
2022-08-16 16:24:25 -0300

    Lógica do logine logout

--- frontend/src/pages/Admin/index.tsx
+++ frontend/src/pages/Admin/index.tsx
@@ -1,5 +1,5 @@
 import PrivateRoute from 'components/PrivateRoute';
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
 import Users from './Auth/User';
 import Navbar from './Navbar';
 
--- frontend/src/util/requests.ts
+++ frontend/src/util/requests.ts
@@ -5,7 +5,7 @@ import jwtDecode from 'jwt-decode'
 
 type Role = 'ROLE_OPERATOR' | 'ROLE_ADMIN';
 
-type TokenData = {
+export type TokenData = {
   exp: number;
   user_name: string;
   authorities: Role[];
@@ -75,6 +75,9 @@ export const getAuthData = () => {
   return JSON.parse(str) as LoginResponse;
 };
 
+export const removeAuthData = () => {
+  localStorage.removeItem(tokenKey);
+}
 
 // Add a request interceptor
 axios.interceptors.request.use(function (config) {
