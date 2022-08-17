6b0d6d39c08376f9ff459f2b6c96d3d719068792
vanessa-maganhoto <vanessammaganhoto@gmail.com>
2022-08-17 16:26:48 -0300

    hasAnyRole

--- frontend/src/pages/Admin/Auth/Login/index.tsx
+++ frontend/src/pages/Admin/Auth/Login/index.tsx
@@ -1,4 +1,4 @@
-import { Link, useHistory } from 'react-router-dom';
+import { Link, useHistory, useLocation } from 'react-router-dom';
 import ButtonIcon from 'components/ButtonIcon';
 import { useForm } from 'react-hook-form';
 
@@ -12,9 +12,18 @@ type FormData = {
   password: string;
 };
 
+type LocationState = {
+  from: string;
+}
+
 const Login = () => {
 
+  const location = useLocation<LocationState>();
+
+  const { from } = location.state || { from: { pathname: '/admin'}};
+
   const { setAuthContextData } = useContext(AuthContext);
+
   const [hasError, setHasError] = useState(false);
 
   const {
@@ -34,7 +43,7 @@ const Login = () => {
           authenticated: true,
           tokenData: getTokenData(),})
 
-        history.push('/admin');
+        history.replace(from);
       })
       .catch((error) => {
         setHasError(true);
--- frontend/src/util/requests.ts
+++ frontend/src/util/requests.ts
@@ -114,3 +114,27 @@ export const isAuthenticated = () : boolean =>{
   const tokenData = getTokenData();
   return (tokenData && tokenData.exp * 1000 > Date.now()) ? true : false;
 }
+
+export const hasAnyRole = (roles: Role[]) : boolean => {
+  if(roles.length === 0){
+    return true;
+  }
+
+  const tokenData = getTokenData();
+
+  /*IMPLEMENTAÇÃO ALTERNATIVA USANDO FUNÇÃO DE ALTA ORDEM 
+    if(tokenData !== undefined){
+      return roles.some(role => tokenData.authorities.includes(roles));
+    }
+  */
+
+  if(tokenData !== undefined){
+    for(var i = 0; i < roles.length; i++){
+      if(tokenData.authorities.includes(roles[i])){
+        return true;
+      }
+    }
+  }
+
+  return false;
+}
