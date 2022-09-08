c4822a0d7e845536db075bbe86fea5a2fa5f28ad
vanessa-maganhoto <vanessammaganhoto@gmail.com>
2022-09-08 13:57:37 -0300

    Rota do formulário de produtos

--- frontend/src/pages/Admin/Products/Form/index.tsx
+++ frontend/src/pages/Admin/Products/Form/index.tsx
@@ -1,41 +1,120 @@
+import { AxiosRequestConfig } from 'axios';
+import { useForm } from 'react-hook-form';
+import { useHistory } from 'react-router-dom';
+import { Product } from 'types/product';
+import { requestBackend } from 'util/requests';
 import './styles.css';
 
 const Form = () => {
+
+  const history = useHistory();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Product>();
+
+  const onSubmit = (formData: Product) => {
+
+    const data = { ...formData, 
+      imgUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQq9YTXllM63X7Tfpos2vty17-gnMfiGTiEBQLzQFH67g&s', 
+      categories: [ { id: 1, name: '' }],
+    }
+
+    const config: AxiosRequestConfig = {
+      method: 'POST',
+      url: '/products',
+      data,
+      withCredentials: true,
+    };
+
+    requestBackend(config).then(() => {
+      history.push("/admin/products");
+    });
+  };
+
+  const handleCancel = () =>{
+    history.push("/admin/products");
+  }
+
   return (
     <div className="product-crud-container">
       <div className="base-card product-crud-form-card">
         <h1 className="product-crud-title">DADOS DO PRODUTO</h1>
 
-        <form>
-          <div className="row products-crud-inputs-container" >
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="row products-crud-inputs-container">
             <div className="col-lg-6 products-crud-inputs-left-container">
               <div className="margin-bottom-30">
-                <input type="text" className="form-control base-input" />
+                <input
+                  {...register('name', {
+                    required: 'Campo obrigatório',
+                  })}
+                  type="text"
+                  className={`form-control base-input ${
+                    errors.name ? 'is-invalid' : ''
+                  }`}
+                  placeholder="Nome do produto"
+                  name="name"
+                />
+                <div className="invalid-feedback d-block">
+                  {errors.name?.message}
+                </div>
               </div>
-
+              
               <div className="margin-bottom-30">
-                <input type="text" className="form-control base-input" />
+                <input
+                  {...register('price', {
+                    required: 'Campo obrigatório',
+                  })}
+                  type="text"
+                  className={`form-control base-input ${
+                    errors.name ? 'is-invalid' : ''
+                  }`}
+                  placeholder="Preço"
+                  name="price"
+                />
+                <div className="invalid-feedback d-block">
+                  {errors.price?.message}
+                </div>
               </div>
-
-              <div className="product-crud-input">
-                <input type="text" className="form-control base-input" />
-              </div>
+              
             </div>
 
             <div className="col-lg-6">
               <div>
                 <textarea
-                  name=""
                   rows={10}
-                  className="form-control base-input h-auto"
-                ></textarea>
+                  {...register('description', {
+                    required: 'Campo obrigatório',
+                  })}
+                  
+                  className={`form-control base-input h-auto ${
+                    errors.name ? 'is-invalid' : ''
+                  }`}
+                  placeholder="Descrição"
+                  name="description"
+                />
+                <div className="invalid-feedback d-block">
+                  {errors.description?.message}
+                </div>
               </div>
             </div>
           </div>
 
           <div className="product-crud-buttons-container">
-            <button className="btn btn-outline-danger product-crud-button"> CANCELAR</button>
-            <button className="btn btn-primary product-crud-button text-white"> SALVAR</button>
+            <button 
+            className="btn btn-outline-danger product-crud-button"
+            onClick={handleCancel}
+            >
+           
+              CANCELAR
+            </button>
+            <button className="btn btn-primary product-crud-button text-white">
+              {' '}
+              SALVAR
+            </button>
           </div>
         </form>
       </div>
