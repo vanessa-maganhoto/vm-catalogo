58862702cd5a1cb6d3be40f868b0bd0fab04e9fa
vanessa-maganhoto <vanessammaganhoto@gmail.com>
2022-10-05 15:35:56 -0300

    MSW mock server, fixtures

--- frontend/src/pages/Catalog/__tests__/Catalog.spec.tsx
+++ frontend/src/pages/Catalog/__tests__/Catalog.spec.tsx
@@ -1,7 +1,12 @@
 import { render, screen, waitFor } from '@testing-library/react';
 import { Router } from 'react-router-dom';
 import history from 'util/history';
+import { server } from './fixtures';
 import Catalog from '..';
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
 
 test('should render Catalog with products', async () => {
   render(
@@ -13,7 +18,6 @@ test('should render Catalog with products', async () => {
   expect(screen.getByText('Catálogo de produtos')).toBeInTheDocument();
 
   await waitFor(() => {
-    expect(screen.getByText('Smart TV')).toBeInTheDocument();
+    expect(screen.getByText('PC Gamer')).toBeInTheDocument();
   });
-
 });
