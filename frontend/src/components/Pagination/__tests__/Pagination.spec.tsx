86faa19c502e1e7be2727df045d77d1664a99f95
vanessa-maganhoto <vanessammaganhoto@gmail.com>
2022-10-05 12:55:20 -0300

    Clicks do componente Pagination

--- frontend/src/components/Pagination/__tests__/Pagination.spec.tsx
+++ frontend/src/components/Pagination/__tests__/Pagination.spec.tsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event'
 
 import Pagination from '..';
 
@@ -28,5 +29,72 @@ describe('Pagination tests', () => {
     expect(page3).not.toHaveClass('pagination-link-active');
 
     expect(page4).not.toBeInTheDocument();
+  });
+
+  test('next arrow should call onChange', () => {
+    // ARANGE
+    const pageCount = 3;
+    const range = 3;
+    const onChange = jest.fn(); 
+    // fn "cria" um objeto fictício para simular o comportamento de uma função
+
+    //ACT
+    render(<Pagination pageCount={pageCount} range={range} onChange={onChange}/>);
+
+    // ASSERT
+
+    const arrowNext = screen.getByTestId("arrow-next");
+
+    userEvent.click(arrowNext);
+    expect(onChange).toHaveBeenCalledWith(1);
+
+  });
+
+  test('previous arrow should call onChange', () => {
+    // ARANGE
+    const pageCount = 3;
+    const range = 3;
+    const onChange = jest.fn(); // fn "cria" um objeto fictício para simular o comportamento de uma função
+    const forcePage = 1;
+
+    //ACT
+    render(
+    <Pagination 
+        pageCount={pageCount} 
+        range={range} 
+        onChange={onChange}
+        forcePage={forcePage}
+    />);
+
+    // ASSERT
+
+    const arrowPrevious = screen.getByTestId("arrow-previous");
+    
+    userEvent.click(arrowPrevious);
+    expect(onChange).toHaveBeenCalledWith(0);
+
+  });
+
+  test('page link should call onChange', () => {
+    // ARANGE
+    const pageCount = 3;
+    const range = 3;
+    const onChange = jest.fn(); 
+
+    //ACT
+    render(
+    <Pagination 
+        pageCount={pageCount} 
+        range={range} 
+        onChange={onChange}
+    />);
+
+    // ASSERT
+
+    const page2 = screen.getByText('2');
+    
+    userEvent.click(page2);
+    expect(onChange).toHaveBeenCalledWith(1);
+
   });
 });
