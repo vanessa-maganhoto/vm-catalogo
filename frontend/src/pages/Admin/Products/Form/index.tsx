44a50a53026f9eafa1f658339dd678c95a4c72a8
vanessa-maganhoto <vanessammaganhoto@gmail.com>
2022-10-18 13:49:07 -0300

    Teste para verificar se apareceu o toast, enviou corretamente o formulário e redirecionou para a página '/admin/products'

--- frontend/src/pages/Admin/Products/Form/index.tsx
+++ frontend/src/pages/Admin/Products/Form/index.tsx
@@ -100,6 +100,7 @@ const Form = () => {
                   }`}
                   placeholder="Nome do produto"
                   name="name"
+                  data-testid= "name"
                 />
                 <div className="invalid-feedback d-block">
                   {errors.name?.message}
@@ -107,6 +108,7 @@ const Form = () => {
               </div>
 
               <div className="margin-bottom-30">
+              <label htmlFor="categories" className="d-none">Categorias</label>
                 <Controller
                   name="categories"
                   rules={{ required: true }}
@@ -121,6 +123,7 @@ const Form = () => {
                       getOptionValue={(category: Category) =>
                         String(category.id)
                       }
+                      inputId="categories"
                     />
                   )}
                 />
@@ -145,6 +148,7 @@ const Form = () => {
                       disableGroupSeparators={true}
                       value={field.value}
                       onValueChange={field.onChange}
+                      data-testid= "price"
                     />
                   )}
                 />
@@ -168,6 +172,7 @@ const Form = () => {
                   }`}
                   placeholder="URL da imagem do produto"
                   name="imgUrl"
+                  data-testid= "imgUrl"
                 />
                 <div className="invalid-feedback d-block">
                   {errors.imgUrl?.message}
@@ -187,6 +192,7 @@ const Form = () => {
                   }`}
                   placeholder="Descrição"
                   name="description"
+                  data-testid= "description"
                 />
                 <div className="invalid-feedback d-block">
                   {errors.description?.message}
---
+++ frontend/src/pages/Admin/Products/__tests__/Form.spec.tsx
@@ -0,0 +1,62 @@
+import { render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { Router, useParams } from 'react-router-dom';
+import history from 'util/history';
+import { server } from './fixtures';
+import selectEvent from 'react-select-event';
+import { ToastContainer } from 'react-toastify';
+
+import Form from '../Form';
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
+
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useParams: jest.fn(),
+}));
+
+describe('Product form create tests', () => {
+  beforeEach(() => {
+    (useParams as jest.Mock).mockReturnValue({
+      productId: 'create',
+    });
+  });
+
+  test('should show toast and redirect when submit form correctly', async () => {
+    render(
+      <Router history={history}>
+        <ToastContainer />
+        <Form />
+      </Router>
+    );
+
+    const nameInput = screen.getByTestId('name');
+    const priceInput = screen.getByTestId('price');
+    const imgUrlInput = screen.getByTestId('imgUrl');
+    const descriptionInput = screen.getByTestId('description');
+    const categoriesInput = screen.getByLabelText('Categorias');
+
+    const submitButton = screen.getByRole('button', { name: /salvar/i });
+
+    await selectEvent.select(categoriesInput, ['Eletrônicos', 'Computadores']);
+    userEvent.type(nameInput, 'Computador');
+    userEvent.type(priceInput, '5000.12');
+    userEvent.type(
+      imgUrlInput,
+      'https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/1-big.jpg'
+    );
+    userEvent.type(descriptionInput, 'Computador muito bom');
+
+    userEvent.click(submitButton);
+
+    await waitFor(() => {
+      const toastElement = screen.getByText('Produto cadastrado com sucesso');
+      expect(toastElement).toBeInTheDocument();
+    });
+
+    expect(history.location.pathname).toEqual('/admin/products')
+
+  });
+});
